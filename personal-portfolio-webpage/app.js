fa6e17d761adac6ef8a4ea347be92df44b1f73f1
Clebson Moura <clebson.sh@gmail.com>
2018-08-27 14:05:16 -0300

    Portifolio Project done

--- personal-portfolio-webpage/app.js
+++
@@ -1,9 +0,0 @@
-
-let nav = document.getElementById('navbar')
-let sticky = nav.offsetTop
-
-window.onscroll = () => {
-  window.pageYOffset >= sticky
-    ? nav.classList.add('sticky')
-    : nav.classList.remove('sticky')
-}
