744edfe266ba371da68c645d20f630faaeb54ca9
Clebson Moura <clebson.sh@gmail.com>
2018-09-07 16:01:07 -0300

    refactored the QuoteBox component

--- random-quote-machine/app.js
+++ random-quote-machine/app.js
@@ -1,47 +1,43 @@
-const Quote = ({quote: {text, author}}) =>
+/* global React ReactDOM */
+const Quote = ({ quote: { text, author } }) =>
   <div id='quote'>
     <p id='text'>{text}</p>
     <p id='author'>- {author}</p>
   </div>
 
 class QuoteBox extends React.Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      quote: {
-        author: '',
-        text: ''
-      },
-      quotes: []
-    }
-
-    this.getRandomQuote = () => {
-      let randomQuoteIndex = Math.floor(Math.random() * this.state.quotes.length)
-      this.setState({
-        quote: this.state.quotes[randomQuoteIndex]
-      })
-    }
+  state = {
+    quote: {
+      author: '',
+      text: ''
+    },
+    quotes: []
   }
 
-  componentDidMount () {
+  getRandomQuote = () => {
+    let randomQuoteIndex = Math.floor(Math.random() * this.state.quotes.length)
+    this.setState({
+      quote: this.state.quotes[randomQuoteIndex]
+    })
+  }
+
+  componentDidMount = () => {
     fetch('https://raw.githubusercontent.com/clebsonsh/freeCodeCamp-projects/master/random-quote-machine/quotes.json')
       .then(data => data.json())
       .then(quotes => {
-        this.setState({quotes})
+        this.setState({ quotes })
         this.getRandomQuote()
       })
   }
 
   render () {
-    let text = this.state.quote.text
-    let author = this.state.quote.author
+    let { quote } = this.state
 
     return (
       <div id='quote-box'>
-        <Quote quote={this.state.quote} />
+        <Quote quote={quote} />
         <div className='btn-group'>
-          <a id='tweet-quote' className='btn' href={`https://twitter.com/intent/tweet?hashtags=freeCodeCamp,randomQuotesMachine&text=${text} - ${author}`}>
+          <a id='tweet-quote' target='_blank' className='btn' href={`https://twitter.com/intent/tweet?hashtags=freeCodeCamp,randomQuotesMachine&text=${quote.text} - ${quote.author}`}>
             Share on Twitter
           </a>
           <a id='new-quote' className='btn' onClick={this.getRandomQuote}>New random quote</a>
