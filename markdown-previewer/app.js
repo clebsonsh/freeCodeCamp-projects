4500f39f847981e1469e865be797bfe0d7187ffb
Clebson Moura <clebson.sh@gmail.com>
2018-09-08 17:09:45 -0300

    Markdown Previewer Project done

--- markdown-previewer/app.js
+++ markdown-previewer/app.js
@@ -1,12 +1,35 @@
 /* global React ReactDOM marked */
 
+const Editor = ({ handleChange, text }) =>
+  <div id='editor-wrapper'>
+    <header id='editor-header'>
+      Editor
+    </header>
+    <textarea
+      id='editor'
+      onChange={handleChange}
+      value={text}
+      rows='15'
+    />
+  </div>
+
+const Preview = ({ text }) =>
+  <div id='preview-wrapper'>
+    <header id='preview-header'>
+      Previewer
+    </header>
+    <div
+      id='preview'
+      dangerouslySetInnerHTML={text}
+    />
+  </div>
+
 // Create a custom filter for links to open them in a new tab
 const renderer = new marked.Renderer()
 renderer.link = (href, title, text) =>
   `<a target='_blank' href='${href}' title='${title}'>${text}</a>`
 
-const markedjs = marked
-markedjs.setOptions({
+marked.setOptions({
   sanitize: true,
   breaks: true,
   renderer: renderer
@@ -26,32 +49,28 @@ const defaultText = `
   ![bacon](https://baconmockup.com/200/200/)
   __tenderloin ground__
 `
+
 class MarkdownPreviewer extends React.Component {
   state = {
     text: defaultText
   }
 
-  handleChange = event => {
-    this.setState({
-      text: event.target.value
-    })
-  }
+  handleChange = event =>
+    this.setState({ text: event.target.value })
 
-  toHTML = str => {
-    return {
-      __html: markedjs(str)
-    }
-  }
+  mdToHTML = str =>
+    ({ __html: marked(str) });
 
-  render = () => {
-    const { text } = this.state
-    return (
-      <div>
-        <textarea id='editor' onChange={this.handleChange} value={text} />
-        <div id='preview' dangerouslySetInnerHTML={this.toHTML(text)} />
-      </div>
-    )
-  }
+  render = () =>
+    <div id='markdown-previewer-wrapper'>
+      <Editor
+        handleChange={this.handleChange}
+        text={this.state.text}
+      />
+      <Preview
+        text={this.mdToHTML(this.state.text)}
+      />
+    </div>
 }
 
 ReactDOM.render(<MarkdownPreviewer />, document.getElementById('root'))
